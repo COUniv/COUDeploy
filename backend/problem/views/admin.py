a458b9552f78347bcc4de4107410672c06a183ab
kdgyun <73222685+kdgyun@users.noreply.github.com>
2022-07-21 00:32:21 +0900

    [BE] get-problemlist 권한 제한 없앰

--- backend/problem/views/admin.py
+++ backend/problem/views/admin.py
@@ -386,8 +386,8 @@ class ProblemAPI(ProblemBase):
         keyword = request.GET.get("keyword", "").strip()
         if keyword:
             problems = problems.filter(Q(title__icontains=keyword) | Q(_id__icontains=keyword))
-        if not user.can_mgmt_all_problem():
-            problems = problems.filter(created_by=user)
+        # if not user.can_mgmt_all_problem():
+        #     problems = problems.filter(created_by=user)
         return self.success(self.paginate_data(request, problems, ProblemAdminSerializer))
 
     @problem_permission_required
