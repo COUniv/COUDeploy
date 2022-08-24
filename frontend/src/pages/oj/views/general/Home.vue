5b0d602b186fff526b9bd00e66db262f8d3e93ae
Doyeon-Kang <z3169@naver.com>
2022-08-24 16:43:04 +0900

    left_announcement tab padding, font-size 조정

--- frontend/src/pages/oj/views/general/Home.vue
+++ frontend/src/pages/oj/views/general/Home.vue
@@ -498,9 +498,9 @@
       margin-bottom: 15px;
       li.tab-item {
         margin-right: 10px;
-        padding: 10px 30px;
+        padding: 10px 24px;
         border-radius: 25px;
-        font-size: 20px;
+        font-size: 18px;
         font-weight: @weight-bold;
         background-color: @light-gray;
         // transition: all 0.2s ease-in-out;
