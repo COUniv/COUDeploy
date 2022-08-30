887d977fbf4d38969c2a117ab43f60f9db36e50e
kdgyun <73222685+kdgyun@users.noreply.github.com>
2022-08-30 22:56:37 +0900

    realtime submissions 수정

--- backend/submission/urls/oj.py
+++ backend/submission/urls/oj.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from ..views.oj import SubmissionAPI, SubmissionListAPI, ContestSubmissionListAPI, SubmissionExistsAPI, SubmissionStatusAPI
+from ..views.oj import SubmissionAPI, SubmissionListAPI, ContestSubmissionListAPI, SubmissionExistsAPI, SubmissionStatusAPI, SafeSubmissionStatusAPI
 
 urlpatterns = [
     url(r"^submission/?$", SubmissionAPI.as_view(), name="submission_api"),
+    url(r"^safe_submission_status/?$", SafeSubmissionStatusAPI.as_view(), name="safe_submission_status_api"),
     url(r"^submission_status/?$", SubmissionStatusAPI.as_view(), name="submission_status_api"),
     url(r"^submissions/?$", SubmissionListAPI.as_view(), name="submission_list_api"),
     url(r"^submission_exists/?$", SubmissionExistsAPI.as_view(), name="submission_exists"),
