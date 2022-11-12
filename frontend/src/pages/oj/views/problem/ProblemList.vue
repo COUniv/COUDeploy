1233226f27f5fced91dd44e685a2a57641136088
kdgyun <73222685+kdgyun@users.noreply.github.com>
2022-11-12 21:40:34 +0900

    [new feature] coloring accepted problems

--- frontend/src/pages/oj/views/problem/ProblemList.vue
+++ frontend/src/pages/oj/views/problem/ProblemList.vue
@@ -56,12 +56,6 @@
                   </Dropdown-menu>
                 </Dropdown>
               </div>
-                                  <!-- <li>
-                                    <i-switch size="large" @on-change="handleTagsVisible">
-                                      <span slot="open">{{$t('m.Tags')}}</span>
-                                      <span slot="close">{{$t('m.Tags')}}</span>
-                                    </i-switch>
-                                  </li> -->
             </div>
             <div class="pick-random">
               <Button long id="pick-one" @click="pickone" type="primary">
@@ -83,7 +77,11 @@
             </thead>
             <tbody v-for="problem in problemList">
               <tr>
-                <td id="problem-id">{{ problem._id }}</td>
+                <td id="problem-id" :class="[(problem.my_status === null ? ''
+                                            : problem.my_status === 0 ? 'accepted-color'
+                                            : problem.my_status === 8 ? 'partially-accepted-color'
+                                            : 'not-accepted-color')]">{{ problem._id }}</td>
+                <!-- <td id="problem-id">{{ problem._id }}</td> -->
                 <td id="problem-title" @click="redirectToProblem(problem)">{{ problem.title }}</td>
                 <td :class="[(problem.difficulty === 'Level1' || problem.difficulty === 'Level2' || 
                               problem.difficulty === 'Level3' || problem.difficulty === 'Level4' || 
@@ -123,77 +121,6 @@
       </div>
     </div>
   </div>
-  <!-- <Row type="flex" :gutter="18" style="margin-left:50px; margin-right:50px">
-    <Col :span="19">
-      <Panel shadow>
-        <div slot="title">{{$t('m.Problem_List')}}</div>
-        <div slot="extra">
-          <ul class="filter">
-            <li>
-              <Dropdown @on-click="filterByDifficulty">
-                <span>{{query.difficulty === '' ? this.$i18n.t('m.Difficulty') : this.$i18n.t('m.' + query.difficulty)}}
-                 <Icon type="arrow-down-b"></Icon> 
-                  <Icon type="md-arrow-dropdown" />
-                </span>
-                <Dropdown-menu slot="list">
-                  <Dropdown-item name="">{{$t('m.All')}}</Dropdown-item>
-                  <Dropdown-item name="Low">{{$t('m.Low')}}</Dropdown-item>
-                  <Dropdown-item name="Mid" >{{$t('m.Mid')}}</Dropdown-item>
-                  <Dropdown-item name="High">{{$t('m.High')}}</Dropdown-item>
-                </Dropdown-menu>
-              </Dropdown>
-            </li> -->
-                                <!-- <li>
-                                  <i-switch size="large" @on-change="handleTagsVisible">
-                                    <span slot="open">{{$t('m.Tags')}}</span>
-                                    <span slot="close">{{$t('m.Tags')}}</span>
-                                  </i-switch>
-                                </li> -->
-            <!-- <li>
-              <Input v-model="query.keyword"
-                   @on-enter="filterByKeyword"
-                   @on-click="filterByKeyword"
-                   placeholder="keyword"
-                   icon="ios-search-strong"/>
-            </li>
-            <li>
-              <Button type="info" @click="onReset">
-                <Icon type="refresh"></Icon>
-                {{$t('m.Reset')}}
-              </Button>
-            </li>
-          </ul>
-        </div>
-        <Table style="width: 100%; font-size: 16px;"
-             :columns="problemTableColumns"
-             :data="problemList"
-             :loading="loadings.table"
-             disabled-hover>
-        </Table>
-      </Panel>
-      <Pagination
-          :total="total" :page-size.sync="query.limit" @on-change="pushRouter" @on-page-size-change="pushRouter" :current.sync="query.page" :show-sizer="true"></Pagination>
-
-    </Col> -->
-
-    <!-- <Col :span="5">
-      <Panel :padding="10">
-        <div slot="title" class="taglist-title">{{$t('m.Tags')}}</div>
-        <Button v-for="tag in tagList"
-            :key="tag.name"
-            @click="filterByTag(tag.name)"
-            :disabled="query.tag === tag.name"
-            shape="circle"
-            class="tag-btn">{{tag.name}}
-        </Button>
-        <Button long id="pick-one" @click="pickone">
-          <Icon type="shuffle"></Icon>
-          {{$t('m.Pick_One')}}
-        </Button>
-      </Panel>
-      <Spin v-if="loadings.tag" fix size="large"></Spin>
-    </Col>
-  </Row>-->
 </template>
 
 <script>
@@ -213,85 +140,6 @@
       return {
         problemCategoryList: {},
         tagList: [],
-        problemTableColumns: [
-          {
-            title: '문제 번호',
-            key: '_id',
-            align: 'center',
-            width: 120,
-            render: (h, params) => {
-              return h('Button', {
-                props: {
-                  type: 'text',
-                  size: 'large'
-                },
-                on: {
-                  click: () => {
-                    this.$router.push({name: 'problem-details', params: {problemID: params.row._id}}).catch(() => {})
-                  }
-                },
-                style: {
-                  padding: '2px 0'
-                }
-              }, params.row._id)
-            }
-          },
-          {
-            title: this.$i18n.t('m.Title'),
-            minWidth: 300,
-            align: 'center',
-            render: (h, params) => {
-              return h('Button', {
-                props: {
-                  type: 'text',
-                  size: 'large'
-                },
-                on: {
-                  click: () => {
-                    this.$router.push({name: 'problem-details', params: {problemID: params.row._id}}).catch(() => {})
-                  }
-                },
-                style: {
-                  padding: '2px 0',
-                  margin: '0 10px',
-                  overflowX: 'auto',
-                  textAlign: 'left',
-                  width: '100%'
-                }
-              }, params.row.title)
-            }
-          },
-          {
-            title: this.$i18n.t('m.Level'),
-            align: 'center',
-            minWidth: 85,
-            render: (h, params) => {
-              let t = params.row.difficulty
-              let color = 'blue'
-              if (t === 'Low') color = 'green'
-              else if (t === 'High') color = 'yellow'
-              return h('Tag', {
-                props: {
-                  color: color
-                }
-              }, this.$i18n.t('m.' + params.row.difficulty))
-            }
-          },
-          {
-            title: '제출',
-            minWidth: 85,
-            align: 'center',
-            key: 'submission_number'
-          },
-          {
-            title: '정답 비율',
-            minWidth: 85,
-            align: 'center',
-            render: (h, params) => {
-              return h('span', this.getACRate(params.row.accepted_number, params.row.submission_number))
-            }
-          }
-        ],
         problemList: [],
         limit: 20,
         total: 0,
@@ -407,28 +255,6 @@
         this.query.page = 1
         this.pushRouter()
       },
-      handleTagsVisible (value) {
-        if (value) {
-          this.problemTableColumns.push(
-            {
-              title: this.$i18n.t('m.Tags'),
-              align: 'center',
-              render: (h, params) => {
-                let tags = []
-                params.row.tags.forEach(tag => {
-                  tags.push(h('Tag', {}, tag))
-                })
-                return h('div', {
-                  style: {
-                    margin: '8px 0'
-                  }
-                }, tags)
-              }
-            })
-        } else {
-          this.problemTableColumns.splice(this.problemTableColumns.length - 1, 1)
-        }
-      },
       onReset () {
         this.$router.push({name: 'problem-list'}).catch(() => {})
       },
@@ -465,6 +291,18 @@
 
 <style scoped lang="less">
   @import '../../../../styles/common.less';
+  .accepted-color {
+    color: green;
+    font-weight: 700;
+  }
+  .partially-accepted-color {
+    color: @deep-dark-orange;
+    font-weight: 700;
+  }
+  .not-accepted-color {
+    color: @red;
+    font-weight: 700;
+  }
   .main-list {
     display: flex;
     margin: 15px;
