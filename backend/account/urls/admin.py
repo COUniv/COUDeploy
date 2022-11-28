51e179eab54fb7f21d38a1f5b357ccb47df6d36b
kdgyun <73222685+kdgyun@users.noreply.github.com>
2022-11-29 01:36:17 +0900

    duplicate router name 해결

--- backend/account/urls/admin.py
+++ backend/account/urls/admin.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r"^user/?$", UserAdminAPI.as_view(), name="user_admin_api"),
     url(r"^generate_user/?$", GenerateUserAPI.as_view(), name="generate_user_api"),
     url(r"^all_managed_user_list/?$", AllManagedUserList.as_view(), name="all_managed_user_list_api"),
-    url(r"^managed_user_list/?$", ManagedUserListAPI.as_view(), name="all_managed_user_list_api"),
+    url(r"^managed_user_list/?$", ManagedUserListAPI.as_view(), name="managed_user_list_api"),
 ]
