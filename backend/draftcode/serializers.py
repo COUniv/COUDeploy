09c3fd62da598729b36fd3fc0c62525ad9f3b0bf
kdgyun <73222685+kdgyun@users.noreply.github.com>
2023-01-07 01:35:15 +0900

    backend 구성

--- backend/draftcode/serializers.py
+++ backend/draftcode/serializers.py
@@ -3,7 +3,7 @@ from .models import DraftCode
 from utils.serializers import LanguageNameChoiceField
 
 class CreateDraftCodeSerializer(serializers.Serializer):
-    problem_id = serializers.IntegerField()
+    problem_id = serializers.CharField(max_length=128)
     contest_id = serializers.IntegerField(required=False)
     language = LanguageNameChoiceField()
 
@@ -14,14 +14,19 @@ class DraftCodeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class UpdateDraftCodeSerializer(serializers.Serializer):
-    problem_id = serializers.IntegerField()
+    problem_id = serializers.CharField(max_length=128)
     contest_id = serializers.IntegerField(required=False)
     language = LanguageNameChoiceField()
     id = serializers.IntegerField()
     checksum = serializers.CharField()
-    code = serializers.CharField(max_length=1024 * 1024)
+    code = serializers.CharField(max_length=1024 * 1024, allow_blank=True)
 
-class DraftCodeSafeSerializer(serializers.ModelSerializer):    
+class DraftCodeSafeSerializer(serializers.ModelSerializer):
     class Meta:
         model = DraftCode
         exclude = ("user", "contest", "problem", "last_update_time")
+
+class DraftLastUpdatedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DraftCode
+        exclude = ("id", "user", "contest", "problem", "language", "checksum", "code")
--- backend/draftcode/views/oj.py
+++ backend/draftcode/views/oj.py
@@ -1,9 +1,10 @@
 from datetime import timezone
-from ..models import DraftCode, Problem, User
+from ..models import DraftCode, Problem, Contest
 from utils.api import APIView, validate_serializer
-from ..serializers import UpdateDraftCodeSerializer, DraftCodeSafeSerializer, CreateDraftCodeSerializer
+from ..serializers import UpdateDraftCodeSerializer, DraftCodeSafeSerializer, CreateDraftCodeSerializer, DraftLastUpdatedSerializer
 from account.decorators import login_required
 from django.db.models import Q
+from hashlib import sha3_256
 
 class DraftCodeAPI(APIView):
 
@@ -11,20 +12,32 @@ class DraftCodeAPI(APIView):
     @login_required
     def post(self, request):
         data = request.data
-        try:
-            problem = Problem.objects.get(id=data["problem_id"], contest_id=data.get("contest_id"), visible=True)
-        except Problem.DoesNotExist:
-            return self.error("존재하지 않은 문제입니다")
-        draft = DraftCode.objects.create(user=request.user, problem=problem, contest=data.get("contest_id"), language=data["language"])
+        if not data.get("contest_id"):
+            try:
+                problem = Problem.objects.get(_id=data["problem_id"], contest_id__isnull=True, visible=True)
+            except Problem.DoesNotExist:
+                return self.error("존재하지 않은 문제입니다")
+            draft = DraftCode.objects.create(user=request.user, problem=problem, language=data["language"])
+        else:
+            try:
+                contest = Contest.objects.get(id=data["contest_id"], visible=True)
+            except Contest.DoesNotExist:
+                return self.error("Contest does not exist")
+            try:
+                problem = Problem.objects.get(_id=data["problem_id"], contest_id=data["contest_id"], visible=True)
+            except Problem.DoesNotExist:
+                return self.error("존재하지 않은 문제입니다")
+            draft = DraftCode.objects.create(user=request.user, problem=problem, contest=contest, language=data["language"])
         draft.save()
+        return self.success("make q")
 
     @login_required
     def get(self, request):
         # contest 는 별도 api로 반환
         if not request.GET.get("problem_id"):
-            return self.error("Invalid Parameter")
+            return self.success("Invalid Parameter")
         if not request.GET.get("language"):
-            return self.error("Invalid Parameter")
+            return self.success("Invalid Parameter")
 
         problem_id = request.GET.get("problem_id")
         language = request.GET.get("language")
@@ -38,9 +51,14 @@ class DraftCodeAPI(APIView):
             try:
                 draft = drafts.get(problem=problem, language=language)
             except DraftCode.DoesNotExist:
-                return self.success("None")
+                #쿼리 악용을 방지하기 위한 hashing 값
+                msg = 'nonedraftcode' + request.user.username
+                h = sha3_256()
+                h.update(msg.encode('utf-8'))
+                return self.success(h.hexdigest())
             result = DraftCodeSafeSerializer(draft).data
             return self.success(result)
+        return self.success("None saved code")
 
     # contest는 별도 api가 관여
     @validate_serializer(UpdateDraftCodeSerializer)
@@ -53,8 +71,6 @@ class DraftCodeAPI(APIView):
             return self.success()
 
         problem_id = data["problem_id"]
-        if data["contest_id"]:
-            return self.error("대회 문제가 아닙니다")
         language = data["language"]
         code = data["code"]
         drafts = DraftCode.objects.filter(user=request.user)
@@ -71,31 +87,46 @@ class DraftCodeAPI(APIView):
                 draft = DraftCode.objects.get(id=id, checksum=checksum)
                 draft.code = code
                 draft.save()
-                return self.success(draft.last_update_time)
+                return self.success(DraftLastUpdatedSerializer(draft).data)
             return self.error("Invalid checksum")
+        return self.success("successed save code")
 
 
 class ContestDraftCodeAPI(APIView):
     @login_required
     def get(self, request):
-
+        # 요청 중 나가는 경우를 생각하여 success로 담음
         if not request.GET.get("problem_id"):
-            return self.error("Invalid Parameter")
+            return self.success("Invalid Parameter")
+        if not request.GET.get("contest_id"):
+            return self.success("Invalid Parameter")
         if not request.GET.get("language"):
-            return self.error("Invalid Parameter")
+            return self.success("Invalid Parameter")
 
         problem_id = request.GET.get("problem_id")
+        contest_id = request.GET.get("contest_id")
         language = request.GET.get("language")
-        contest = self.contest
+        try:
+            contest = Contest.objects.get(id=contest_id, visible=True)
+        except Contest.DoesNotExist:
+            return self.error("Contest does not exist")
         drafts = DraftCode.objects.filter(user=request.user)
         if problem_id:
             try:
-                problem = Problem.objects.get(_id=problem_id, contest_id=contest.id, visible=True)
+                problem = Problem.objects.get(_id=problem_id, contest_id=contest_id, visible=True)
             except Problem.DoesNotExist:
                 return self.error("문제가 존재하지 않습니다")
-            draft = drafts.get(problem=problem, language=language)
+            try:
+                draft = drafts.get(problem=problem, contest=contest, language=language)
+            except DraftCode.DoesNotExist:
+                #쿼리 악용을 방지하기 위한 hashing 값
+                msg = 'nonedraftcode' + request.user.username
+                h = sha3_256()
+                h.update(msg.encode('utf-8'))
+                return self.success(h.hexdigest())
             result = DraftCodeSafeSerializer(draft).data
             return self.success(result)
+        return self.success("None saved code")
 
     @validate_serializer(UpdateDraftCodeSerializer)
     @login_required
@@ -109,21 +140,27 @@ class ContestDraftCodeAPI(APIView):
         problem_id = data["problem_id"]
         if not data["contest_id"]:
             return self.error("Invalid parameter")
+        contest_id = data["contest_id"]
         language = data["language"]
         code = data["code"]
         drafts = DraftCode.objects.filter(user=request.user)
+        try:
+            contest = Contest.objects.get(id=contest_id, visible=True)
+        except Contest.DoesNotExist:
+            return self.error("Contest does not exist")
         if problem_id:
             try:
                 problem = Problem.objects.get(_id=problem_id, contest_id=data["contest_id"], visible=True)
             except Problem.DoesNotExist:
                 return self.error("문제가 존재하지 않습니다")
             try:
-                draft = drafts.get(problem=problem, language=language)
+                draft = drafts.get(problem=problem, contest=contest, language=language)
             except DraftCode.DoesNotExist:
                 return self.success("임시저장이 존재하지 않습니다")
             if draft.id == id and draft.checksum == checksum:
                 draft = DraftCode.objects.get(id=id, checksum=checksum)
                 draft.code = code
                 draft.save()
-                return self.success(draft.last_update_time)
+                return self.success(DraftLastUpdatedSerializer(draft).data)
             return self.error("Invalid checksum")
+        return self.success("successed save code")
